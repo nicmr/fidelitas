191566cca63f6b06d6e38d63a54e5a2c4b7f645b
Nicolas Mohr <Nico.Mohr@gmx.net>
2019-09-21 02:37:18 +0200

    implemented global websocket registration, refactored & deprecated http api

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpRequest, HttpServer};
 use actix_files::NamedFile;
-use actix_web::{HttpRequest, Result};
 use actix_web_actors::ws;
-use actix::{Actor, StreamHandler};
+use actix::{Actor, StreamHandler, Addr};
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use vlc;
 use std::thread;
 
@@ -13,8 +12,13 @@ use crossbeam_channel;
 use lazy_static;
 
 
+// Http API, deprecated in favour of websocket api
+mod httpapi;
 
-struct AppState {
+
+type BasicError = &'static str;
+
+pub struct AppState {
     sender: crossbeam_channel::Sender<PlayerMessage>,
 }
 
@@ -22,7 +26,8 @@ enum PlayerMessage {
     Play(String),
     Pause,
     Resume,
-    Stop
+    Stop,
+    Register(Addr<PlayerWs>)
 }
 
 impl std::convert::TryFrom<String> for PlayerMessage {
@@ -54,14 +59,14 @@ impl std::convert::TryFrom<String> for PlayerMessage {
 }
 
 
-fn index(_req: HttpRequest) -> Result<NamedFile> {
+fn index(_req: HttpRequest) -> actix_web::Result<NamedFile> {
     // let path: PathBuf = req.match_info().query("~/Code/rust/fidelitas/index.html").parse().unwrap();
     let path: PathBuf = PathBuf::from("index.html");
     
     Ok(NamedFile::open(path)?)
 }
 
-fn controls(_req: HttpRequest) -> Result<NamedFile> {
+fn controls(_req: HttpRequest) -> actix_web::Result<NamedFile> {
     // let path: PathBuf = req.match_info().query("~/Code/rust/fidelitas/index.html").parse().unwrap();
     let path: PathBuf = PathBuf::from("controls.js");
     
@@ -76,7 +81,32 @@ impl Actor for PlayerWs {
     type Context = ws::WebsocketContext<Self>;
 }
 
+impl actix::Handler<WsOutgoingMsg> for PlayerWs {
+    type Result = Result<(), BasicError>;
+    fn handle(&mut self, msg: WsOutgoingMsg, ctx: &mut Self::Context) -> Self::Result {
+
+        ctx.text(msg.text);
+        Ok(())
+    }
+}
+
 impl StreamHandler<ws::Message, ws::ProtocolError> for PlayerWs {
+    fn started(&mut self, ctx: &mut Self::Context) {
+        // bring trait into scope for access to ctx.address()
+        use actix::AsyncContext;
+
+        let addr = ctx.address();
+        match self.sender.send(PlayerMessage::Register(addr)) {
+            Ok(()) => {
+                ctx.text("Ws registered");
+                println!("Ws registered");
+            },
+            Err(e) => {
+                ctx.text("Ws registration failed");
+                println!("Ws registration failed: {}", e);
+            },
+        }
+    }
     fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
         use std::convert::TryFrom;
 
@@ -95,68 +125,51 @@ impl StreamHandler<ws::Message, ws::ProtocolError> for PlayerWs {
             _ => (),
         }
     }
+    fn finished(&mut self, ctx: &mut Self::Context) {
+        //bring traits into scope for access to ctx.address() and ctx.stop()
+        use actix::AsyncContext;
+        use actix::ActorContext;
+
+        let addr = ctx.address();
+        match self.sender.send(PlayerMessage::Register(addr)) {
+            Ok(()) => {
+                println!("Ws unregistered");
+            },
+            Err(e) => {
+                println!("Ws unregistration failed: {}", e);
+            },
+        }
+        ctx.stop();
+    }
 }
 
-fn api_websocket((req, state): (HttpRequest, web::Data<AppState>), stream: web::Payload) -> Result<HttpResponse, actix_web::Error> {
+
+struct WsOutgoingMsg {
+    text: String,
+}
+
+
+impl actix::Message for WsOutgoingMsg {
+    type Result = Result<(), BasicError>;
+}
+
+
+fn valid_directory(s: String) -> Result<(), String>{
+    let path = Path::new(&s);
+    if path.is_dir() {
+        Ok(())
+    } else {
+        Err(String::from("Not a valid path to a directory"))
+    }
+    
+}
+
+fn api_websocket((req, state): (HttpRequest, web::Data<AppState>), stream: web::Payload) -> actix_web::Result<HttpResponse, actix_web::Error> {
     let resp = ws::start(PlayerWs{ sender: state.sender.clone() }, &req, stream);
     println!("{:?}", resp);
     resp
 }
 
-// fn api_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-//     let resp = ws::start();
-//     resp
-// }
-
-fn api_play((_req, state): (HttpRequest, web::Data<AppState>)) -> impl Responder {
-
-    let track_name = String::from("Xi-FreedomDive.mp3");
-
-    match state.sender.send(PlayerMessage::Play(track_name.clone())) {
-        Ok(_) => {
-            format!("Track name sent succesfully: {}", track_name)
-        },
-        Err(e) => {
-            format!("Failed to send track: {}", e)
-        }
-    }
-}
-
-fn api_pause((_req, state): (HttpRequest, web::Data<AppState>)) -> impl Responder {
-    
-    match state.sender.send(PlayerMessage::Pause) {
-        Ok(_) => {
-            format!("Pause message sent successfully")
-        },
-        Err(e) => {
-            format!("Failed to send pause message: {}", e)
-        }
-    }
-}
-
-fn api_stop((_req, state): (HttpRequest, web::Data<AppState>)) -> impl Responder {
-    
-    match state.sender.send(PlayerMessage::Stop) {
-        Ok(_) => {
-            format!("Stop message sent successfully")
-        },
-        Err(e) => {
-            format!("Failed to send pause message: {}", e)
-        }
-    }
-}
-
-fn api_resume((_req, state): (HttpRequest, web::Data<AppState>)) -> impl Responder {
-    
-    match state.sender.send(PlayerMessage::Resume) {
-        Ok(_) => {
-            format!("Pause message sent successfully")
-        },
-        Err(e) => {
-            format!("Failed to send pause message: {}", e)
-        }
-    }
-}
 
 fn main() {
 
@@ -165,16 +178,33 @@ fn main() {
         .author("Nicolas Mohr <Nico.Mohr@gmx.net")
         .about("Network audio player")
         .arg(clap::Arg::with_name("port")
+            .takes_value(true)
             .default_value("8088")
             .short("p")
             .long("port")
             .value_name("PORT")
-            .help("Specifies the port the server will listen on")
-            .takes_value(true))
+            .help("The port the server will listen on")
+            )
+        .arg(clap::Arg::with_name("dir")
+            .takes_value(true)
+            .default_value("./music")
+            .short("d")
+            .long("dir")
+            .value_name("PATH")
+            .help("The directory whose files will be available for playback.")
+            .validator(valid_directory)
+            )
         .get_matches();
+
+
+    let path = Path::new(matches.value_of("dir").unwrap());
+    println!("Hosting files in folder: {}", path.to_str().unwrap());
+
 
     let port = matches.value_of("port").unwrap();
     println!("Listening on port: {}...", port);
+
+    
 
 
     let (sender, receiver) = crossbeam_channel::unbounded();
@@ -182,6 +212,9 @@ fn main() {
     let _handle = thread::spawn(move || {
         let vlc_instance = vlc::Instance::new().unwrap();
         let mediaplayer = vlc::MediaPlayer::new(&vlc_instance).unwrap();
+
+        let mut ws_connections: Vec::<Addr<PlayerWs>> = Vec::new();
+
         loop {
             match receiver.recv() {
                 Ok(msg) => {
@@ -194,12 +227,25 @@ fn main() {
                         },
                         PlayerMessage::Pause => {
                             mediaplayer.pause();
+                            for conn in &mut ws_connections {
+                                match conn.try_send(
+                                    WsOutgoingMsg{
+                                        text: String::from("Somebody else paused.")
+                                    }
+                                ){
+                                    Ok(_) => {},
+                                    Err(_) => {println!("Failed to send: ")}
+                                }
+                            }
                         },
                         PlayerMessage::Resume => {
                             mediaplayer.play().unwrap();
                         },
                         PlayerMessage::Stop => {
                             mediaplayer.stop();
+                        },
+                        PlayerMessage::Register(ws) => {
+                            ws_connections.push(ws);
                         },
                     }
                 },
@@ -221,11 +267,11 @@ fn main() {
             )
             .service(
                 web::scope("api")
-                    .route("play/{file}", web::get().to(api_play))
-                    .route("pause", web::get().to(api_pause))
-                    .route("resume", web::get().to(api_resume))
-                    .route("stop", web::get().to(api_stop))
                     .route("ws", web::get().to(api_websocket))
+                    .route("play/{file}", web::get().to(httpapi::play))
+                    .route("pause", web::get().to(httpapi::pause))
+                    .route("resume", web::get().to(httpapi::resume))
+                    .route("stop", web::get().to(httpapi::stop))
             )
             .service(
                 web::scope("static")
